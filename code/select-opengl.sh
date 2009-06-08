fa9332324060852a5fa89c10d51a5f59b7f8f127
jensp <jens@jenux.homelinux.org>
2009-06-08 17:50:45 +0200

    some cleanups and bugfixes

--- code/select-opengl.sh
+++ code/select-opengl.sh
@@ -8,19 +8,26 @@ _usage() {
 if [ "$#" -lt 1 ]
   then
     _usage
+    exit 1
 fi
 if [ "$1" == "list" ]
 then
-  ls /usr/share/select-opengl/functions/* |sed -e 's/-symlinks.sh//g'
+  for func in /usr/share/select-opengl/functions/*
+  do
+    ls $func |sed -e 's/-symlinks.sh//g'|xargs basename
+  done
 elif [ "$1" == "set" ]
+then
   if [ -z $2 ]
   then
     echo "you must specify an opengl implementation"
-  elif [ -e /usr/share/select/opengl/functions/$2-symlinks.sh ]
-    source /usr/share/select/opengl/functions/$2-symlinks.sh
+  elif [ -e /usr/share/select-opengl/functions/$2-symlinks.sh ]
+  then
+    source /usr/share/select-opengl/functions/$2-symlinks.sh
   else 
     echo "implementation doesn't exist, check $0 list"
   fi
 else
-  _usage 
+  _usage
+  exit 1
 fi
