ac9880cbe758429ac3f5d47f9ef58bae5a6adf97
jensp <jens@jenux.homelinux.org>
2009-06-09 13:36:51 +0200

    made nvidia-173xx select-opengl compatible, some cleanups to nvidia

---
+++ pkgbuilds/x11/nvidia-173xx-utils/nvidia-173xx-symlinks.sh
@@ -0,0 +1,33 @@
+for lib in /usr/lib/nvidia-173xx/*.so*
+do
+  if [ -h /usr/lib/$(basename "$lib") ]
+  then
+    ln -sf $lib /usr/lib/$(basename "$lib")
+  elif [ ! -e /usr/lib/$(basename "$lib") ]
+  then
+    ln -sf $lib /usr/lib/$(basename "$lib")
+  else
+    echo "there is already a library of the same name available, bailing out" && return 2
+  fi
+done
+if [ -h /usr/lib/xorg/modules/drivers/nvidia_drv.so ]
+then
+  ln -sf /usr/lib/nvidia-173xx/driver/nvidia_drv.so /usr/lib/xorg/modules/drivers/nvidia_drv.so
+elif [ ! -e /usr/lib/xorg/modules/drivers/nvidia_drv.so ]
+then
+  ln -sf /usr/lib/nvidia-173xx/driver/nvidia_drv.so /usr/lib/xorg/modules/drivers/nvidia_drv.so
+else
+  echo "there is already a library of the same name available, bailing out" && return 2
+fi
+for ext in /usr/lib/nvidia-173xx/modules/extensions/*.so*
+do
+  if [ -h /usr/lib/xorg/modules/extensions/$(basename $ext) ]
+  then
+    ln -sf $ext /usr/lib/xorg/modules/extensions/$(basename $ext)
+  elif [ ! -e /usr/lib/xorg/modules/extensions/$(basename $ext) ]
+  then
+    ln -sf $ext /usr/lib/xorg/modules/extensions/$(basename $ext)
+  else
+    echo "there is already a library of the same name available, bailing out" && return 2
+  fi
+done
