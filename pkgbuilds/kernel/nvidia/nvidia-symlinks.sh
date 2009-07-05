bf79d76c17a45f2c45142cb8bd25f4d5de1f82e7
jensp <jens@jenux.homelinux.org>
2009-07-06 00:00:18 +0200

    another bunch of changes due to chaox-opengl-tools (cherry picked from commit d953d65da1af71277c220467d44676fb6b1f5f95)

--- pkgbuilds/kernel/nvidia/nvidia-symlinks.sh
+++ pkgbuilds/kernel/nvidia/nvidia-symlinks.sh
@@ -4,20 +4,20 @@ for lib in /usr/lib/nvidia/*.so*
 do
   if [ -h /usr/lib/$(basename "$lib") ]
   then
-    _ln $lib /usr/lib/$(basename "$lib") $_symlist
+    _ln $lib /usr/lib/$(basename "$lib") 
   elif [ ! -e /usr/lib/$(basename "$lib") ]
   then
-    _ln $lib /usr/lib/$(basename "$lib") $_symlist
+    _ln $lib /usr/lib/$(basename "$lib") 
   else
     echo "there is already a library of the same name available, bailing out" && return 2
   fi
 done
 if [ -h /usr/lib/xorg/modules/drivers/nvidia_drv.so ]
 then
-  _ln /usr/lib/nvidia/modules/drivers/nvidia_drv.so /usr/lib/xorg/modules/drivers/nvidia_drv.so $_symlist
+  _ln /usr/lib/nvidia/modules/drivers/nvidia_drv.so /usr/lib/xorg/modules/drivers/nvidia_drv.so 
 elif [ ! -e /usr/lib/xorg/modules/drivers/nvidia_drv.so ]
 then
-  _ln /usr/lib/nvidia/modules/drivers/nvidia_drv.so /usr/lib/xorg/modules/drivers/nvidia_drv.so $_symlist
+  _ln /usr/lib/nvidia/modules/drivers/nvidia_drv.so /usr/lib/xorg/modules/drivers/nvidia_drv.so 
 else
   echo "there is already a library of the same name available, bailing out" && return 2
 fi
@@ -25,21 +25,21 @@ for ext in /usr/lib/nvidia/modules/extensions/*.so*
 do
   if [ -h /usr/lib/xorg/modules/extensions/$(basename $ext) ]
   then
-    _ln $ext /usr/lib/xorg/modules/extensions/$(basename $ext) $_symlist
+    _ln $ext /usr/lib/xorg/modules/extensions/$(basename $ext) 
   elif [ ! -e /usr/lib/xorg/modules/extensions/$(basename $ext) ]
   then
-    _ln $ext /usr/lib/xorg/modules/extensions/$(basename $ext) $_symlist
+    _ln $ext /usr/lib/xorg/modules/extensions/$(basename $ext) 
   else
     echo "there is already a library of the same name available, bailing out" && return 2
   fi
 done
 if [ -h /lib/modules/$(uname -r)/kernel/drivers/video/nvidia.ko ]
 then
-  _ln /usr/lib/nvidia/modules/$(uname -r)/nvidia.ko /lib/modules/$(uname -r)/kernel/drivers/video/nvidia.ko $_symlist
+  _ln /usr/lib/nvidia/modules/$(uname -r)/nvidia.ko /lib/modules/$(uname -r)/kernel/drivers/video/nvidia.ko
   depmod -A
 elif [ ! -e /lib/modules/$(uname -r)/kernel/drivers/video/nvidia.ko ]
 then
-  _ln /usr/lib/nvidia/modules/$(uname -r)/nvidia.ko /lib/modules/$(uname -r)/kernel/drivers/video/nvidia.ko $_symlist
+  _ln /usr/lib/nvidia/modules/$(uname -r)/nvidia.ko /lib/modules/$(uname -r)/kernel/drivers/video/nvidia.ko
   depmod -A
 else
   echo "there is already a module of the same name available, bailing out" && exit 2
