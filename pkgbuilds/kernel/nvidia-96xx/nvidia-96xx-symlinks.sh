69f431ed18d7cec475101ff4aeac8be122029840
jensp <jens@jenux.homelinux.org>
2009-08-03 21:12:14 +0200

    make libdri.so symlink when setting nvidia symlinks

--- pkgbuilds/kernel/nvidia-96xx/nvidia-96xx-symlinks.sh
+++ pkgbuilds/kernel/nvidia-96xx/nvidia-96xx-symlinks.sh
@@ -44,3 +44,13 @@ then
 else
   echo "there is already a module of the same name available, bailing out" && exit 2
 fi
+if [ -h /usr/lib/xorg/modules/extensions/libdri.so ]
+then
+  _ln /usr/lib/xorg/modules/_extensions/libdri.so /usr/lib/xorg/modules/extensions/libdri.so
+elif [ ! -e /usr/lib/xorg/modules/extensions/libdri.so ]
+then
+  _ln /usr/lib/xorg/modules/_extensions/libdri.so /usr/lib/xorg/modules/extensions/libdri.so
+else
+  echo "there is already a library of the same name available, bailing out" && return 2
+fi
+
